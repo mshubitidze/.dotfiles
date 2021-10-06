2626492c445ffcf205fb97fef13a1af1f558bef9
Mikheil Shubitidze <msh@mook.local>
2021-10-06 04:02:18 +0400

    add new config files

---
+++ config/init.zsh
@@ -0,0 +1,16 @@
+export ZSH="/Users/msh/.oh-my-zsh"
+
+ZSH_THEME="random"
+
+# plugins
+plugins=(git)
+
+source $ZSH/oh-my-zsh.sh
+
+# aliases
+alias vim="nvim"
+alias zshconfig="vim ~/.dotfiles/config/init.zsh"
+alias vimconfig="vim ~/.dotfiles/config/init.vim"
+alias ohmyzsh="vim ~/.oh-my-zsh"
+alias sl="ls"
+alias code="/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code"
